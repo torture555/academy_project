ddd9c6af6a2c203b8ebb7712561904ce938d967c
torture555 <pashast1999@gmail.com>
2022-05-01 12:54:41 +0300

    add comments and closing file after fill slice with words

--- main.go
+++ main.go
@@ -16,10 +16,10 @@ func main() {
 
 		var inputValue = sc.Text()
 
-		if inputValue == "exit" { //exit from programm
+		if inputValue == "exit" { // exit from programm
 			fmt.Println("Bye!")
 			break
-		} else if strings.HasSuffix(inputValue, ".txt") {
+		} else if strings.HasSuffix(inputValue, ".txt") { // if input file name fill tabooWords
 
 			tabooWords = nil // clear old slice with taboo words
 
@@ -29,7 +29,7 @@ func main() {
 				break
 			}
 
-			scanTxt := bufio.NewScanner(file) //scaning file and append taboo words in slice
+			scanTxt := bufio.NewScanner(file) //scaning file and append taboo words in slice tabooWords
 			for scanTxt.Scan() {
 				if strings.HasPrefix(scanTxt.Text(), ">") {
 					continue
@@ -37,17 +37,23 @@ func main() {
 				tabooWords = append(tabooWords, strings.ToLower(scanTxt.Text()))
 			}
 
+			errCloseFile := file.Close() // close file with taboo words
+			if errCloseFile != nil {
+				fmt.Println(errCloseFile)
+				break
+			}
+
 		} else {
 
-			outputPrint := inputValue
+			outputPrint := inputValue // processing input value
 			tmpStr := outputPrint
 			tmpStr = strings.ToLower(tmpStr)
 
-			for _, tabooWord := range tabooWords {
+			for _, tabooWord := range tabooWords { // if temp string with lower letters have taboo words replace on asterisks
 				tmpStr = strings.ReplaceAll(tmpStr, strings.ToLower(tabooWord), getAsteriskWord(tabooWord))
 			}
 
-			if strings.Contains(tmpStr, "*") {
+			if strings.Contains(tmpStr, "*") { // comparison and replace taboo words in output string letter by letter
 				for i, letter := range tmpStr {
 					if letter == rune(42) {
 						outputPrint = outputPrint[:i] + "*" + outputPrint[i+1:]
@@ -55,12 +61,12 @@ func main() {
 				}
 			}
 
-			fmt.Println(outputPrint)
+			fmt.Println(outputPrint) // print result
 		}
 	}
 }
 
-func getAsteriskWord(str string) string {
+func getAsteriskWord(str string) string { // calculate length word and create string same length filling asterisks
 	outputStr := ""
 	for i := 0; i < len(str); i++ {
 		outputStr += "*"
